b1eaa9e0318814658839ec744d2a58dcd0ded49f
westbrookjc <westbrookjamesc@gmail.com>
2017-10-05 09:27:47 -0400

    Filled out some copy

--- idd/mainlanding/views.py
+++ idd/mainlanding/views.py
@@ -4,5 +4,5 @@ def index(request):
         return render(request, 'mainlanding/home.html')
 
 def about(request):
-        return render(request, 'mainlanding/basic.html', {'content':['Hello! I\'m an engineering graduate with a passion for data. If you would like to contact me, please email me at:','westbrookjamesc@gmail.com']})
+        return render(request, 'mainlanding/basic.html')
 
