b9466e3669dce1bd886e249e0694df6967c9ba71
westbrookjc <westbrookjamesc@gmail.com>
2017-10-16 16:00:36 -0400

    Added time/date importing from django.utils

--- idd/stories/models.py
+++ idd/stories/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=180)
     body = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
             return self.title
